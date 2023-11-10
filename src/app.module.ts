7a0a753aa81fd292de75a1c977fb5f7914c3326d
hugocruzlfc <hugocruzlfc@gmail.com>
2023-11-10 17:09:16 +0100

    feat: adding nest config and enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       port: 3306,
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
-      database: 'test',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      database: process.env.MYSQL_DATABASE,
     }),
     UsersModule,
   ],
